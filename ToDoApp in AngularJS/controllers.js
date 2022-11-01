8120ce27c83232961f5207328801e5de8088f7f6
won369369 <60108250+won369369@users.noreply.github.com>
2022-11-01 16:44:35 -0400

    Add files via upload

---
+++ ToDoApp in AngularJS/controllers.js
@@ -0,0 +1,17 @@
+import angular from 'angular';
+angular.module('todo').controller('TodoCtrl', function ($scope, todoStorage) {
+  $scope.todos = todoStorage.get();
+
+  $scope.remove = function (todo) {
+    todoStorage.remove(todo);
+  };
+
+  $scope.add = function (newTodoTitle) {
+    todoStorage.add(newTodoTitle);
+    $scope.newTodoTitle = '';
+  };
+  
+  $scope.update = function(){
+    todoStorage.update();
+  }
+});
---
+++ ToDoApp in AngularJS/services.js
@@ -0,0 +1,41 @@
+import angular from 'angular';
+angular.module('todo').factory('todoStorage', function () {
+  var TODO_DATA = 'TODO_DATA';
+  var storage = {
+    todos: [],
+    _saveToLocalStorage: function (data) {
+      localStorage.setItem(TODO_DATA, JSON.stringify(data));
+    },
+    _getFromLocalStorage: function () {
+      return JSON.parse(localStorage.getItem(TODO_DATA)) || []; 
+    },
+    get: function () {
+      angular.copy(storage._getFromLocalStorage(), storage.todos);
+      return storage.todos;
+    },
+    remove: function (todo) {
+      var idx = storage.todos.findIndex(function (item) {
+        return item.id === todo.id;
+      });
+
+      if (idx > -1) {
+        storage.todos.splice(idx, 1);
+        storage._saveToLocalStorage(storage.todos);
+      }
+    },
+    add: function (newTodoTitle) {
+      var newTodo = {
+        title: newTodoTitle,
+        completed: false,
+        createdAt: Date.now(),
+      };
+
+      storage.todos.push(newTodo);
+      storage._saveToLocalStorage(storage.todos);
+    },
+    update: function(){
+      storage._saveToLocalStorage(storage.todos);
+    }
+  }
+  return storage;
+});
